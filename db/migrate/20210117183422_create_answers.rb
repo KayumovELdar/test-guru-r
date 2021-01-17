ef9df9f9b97c1b6919e9f0daaf60da027e0350a7
KayumovELdar <eldar13121997@mail.ru>
2021-01-17 21:55:19 +0300

    migrate

--- db/migrate/20210117183422_create_answers.rb
+++ db/migrate/20210117183422_create_answers.rb
@@ -1,7 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.string :text
+      t.string :text, null: false
+      t.references :question, null: false, foreign_key: true
+      t.boolean :correct, default: false, null: false
+
 
       t.timestamps
     end
