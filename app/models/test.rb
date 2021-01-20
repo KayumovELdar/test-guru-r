0559cc641f0f554fa856b9a91293f025a5f043d8
KayumovELdar <eldar13121997@mail.ru>
2021-01-21 00:28:01 +0300

    создание основных операций
    1)создание инстанс метода в User
    2)создание метода класса Test
    3)заполнение seed

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.from_category(category_name)
+    Test.joins("JOIN categories ON categories.id =tests.category_id")
+        .where("categories.name = ?", category_name).order(id: :desc).pluck(:name)
+  end
 end
