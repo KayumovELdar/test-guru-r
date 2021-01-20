9b83a8d71a134f52e8d8e1debc08fbe433a0af46
KayumovELdar <eldar13121997@mail.ru>
2021-01-20 23:51:38 +0300

    version 0.30

--- db/migrate/20210120204354_create_base_user_tests.rb
+++ db/migrate/20210120204354_create_base_user_tests.rb
@@ -1,6 +1,8 @@
 class CreateBaseUserTests < ActiveRecord::Migration[6.1]
   def change
     create_table :base_user_tests do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :test, null: false, foreign_key: true
 
       t.timestamps
     end
