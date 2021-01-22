9fda680fa1a91be59a7823c5ee590bc2294f7d74
KayumovELdar <eldar13121997@mail.ru>
2021-01-22 21:50:50 +0300

    правка

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
 
   belongs_to :category
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
   has_many :questions, dependent: :destroy
   has_many :user_tests
   has_many :users, through: :user_tests, dependent: :destroy
