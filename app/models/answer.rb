8c92e0f9f6a765cf5d1e73397dca1ff56f283e71
KayumovELdar <eldar13121997@mail.ru>
2021-01-25 13:38:39 +0300

    добавление ограничения по числу ответов

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,9 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :text, presence: true
+  validate :validate_length, on: :create
+
+  def validate_length
+    errors.add(:question) if question.answers.count >= 4
+  end
 end
