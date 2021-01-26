a2c4ea0a6d46df2d50e03ba77de9413015a42ef9
KayumovELdar <eldar13121997@mail.ru>
2021-01-27 02:30:05 +0300

    правка

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,7 +8,7 @@ class Answer < ApplicationRecord
 
   def validate_length
     if question.answers.count >= 4
-      puts "Число ответов не может быть выше 4"
+      # число ответов не может быть больше 4
       errors.add(:question)
     end
   end
