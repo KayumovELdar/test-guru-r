6f6b8ebe892e982915e367011a1c9a8418835e41
KayumovELdar <eldar13121997@mail.ru>
2021-01-28 18:45:21 +0300

    index create show destroy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,41 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :resque_with_test_not_found
+
+  def index
+      render html: @test.questions
+  end
+
+  def show
+    question = @question
+
+     render html: "Question: #{question.text}<br>id: #{question.id} <br>".html_safe
+  end
+
+  def new
+
+  end
+
+  def create
+    question = Question(params.require(:question).permit(:text))
+
+    render plain:test.inspect
+  end
+  def destroy
+    @questio.destroy
+    render html: "Вопрос удалён!"
+  end
+  def find_question
+   @question = Question.find(params[:id])
+ end
+
+ def find_test
+   @test = Test.find(params[:id])
+ end
+
+ def resque_with_test_not_found
+    render html: 'Пусто'
+  end
 end
