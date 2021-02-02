83d6d490c1920258f9068f7f67bea05190e5ca4b
KayumovELdar <eldar13121997@mail.ru>
2021-02-02 06:30:26 +0300

    правка

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -8,41 +8,41 @@ class TestsController < ApplicationController
   end
 
   def new
-      @test = Test.new
+    @test = Test.new
+    
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
     end
+  end
 
-    def create
-      @test = Test.new(test_params)
+  def edit
+    @test = Test.find(params[:id])
+  end
 
-      if @test.save
-        redirect_to @test
-      else
-        render :new
-      end
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
     end
+  end
 
-    def edit
-      @test = Test.find(params[:id])
+  def destroy
+    @test = Test.find(params[:id])
+    @test.destroy
+    redirect_to root_path
+  end
+
+  private
+    def test_params
+      params.require(:test).permit(:title, :level)
     end
-
-    def update
-      @test = Test.find(params[:id])
-
-      if @test.update(test_params)
-        redirect_to @test
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @test = Test.find(params[:id])
-      @test.destroy
-      redirect_to root_path
-    end
-
-    private
-      def test_params
-        params.require(:test).permit(:name, :level)
-      end
   end
