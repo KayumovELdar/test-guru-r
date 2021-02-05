e49a5faae851c8fecc7229d8a3fbf3b635b8aefb
KayumovELdar <eldar13121997@mail.ru>
2021-02-05 21:01:22 +0300

    migrate

--- db/migrate/20210205174336_create_tests.rb
+++ db/migrate/20210205174336_create_tests.rb
@@ -1,6 +1,10 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
+      t.string :name, null: false
+      t.integer :level, default: 0, null: false
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
