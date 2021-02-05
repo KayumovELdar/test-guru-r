8f2dad084e00f9e31fc6543f351f364d5cf56396
KayumovELdar <eldar13121997@mail.ru>
2021-02-05 21:14:18 +0300

    validate and test_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :tests, foreign_key: :author_id, dependent: :destroy
+  has_many :test_taking_sessions, dependent: :destroy
+  has_many :taken_tests, through: :test_taking_sessions, source: :test
+
+  validates :name, presence: true
+
+  def participated_tests(level)
+    taken_tests.of_level(level)
+  end
 end
