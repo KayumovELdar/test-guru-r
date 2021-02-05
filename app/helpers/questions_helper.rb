86ae309319f83a4ead850015d511f9ec4d05b3e2
KayumovELdar <eldar13121997@mail.ru>
2021-02-05 10:14:24 +0300

    Update questions_helper.rb

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
+
+  def question_header(question)
+  if question.persisted?
+    'Редактировать вопрос'
+  else
+    'Создать новый вопрос'
+  end
+end
 end
