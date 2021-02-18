aa73b8315dba50be6857426b1c3f336d58000769
KayumovELdar <eldar13121997@mail.ru>
2021-02-18 18:51:42 +0300

    Update test_passages_controller.rb

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -17,21 +17,6 @@ class TestPassagesController < ApplicationController
     end
   end
 
-  def gist
-  result = GistQuestionService.new(@test_passage.current_question).call
-  if result.success?
-      current_user.gists.create!(question: @test_passage.current_question,
-                                 user: current_user,
-                                 url: result.url)
-
-      flash[:notice] = t('.success', result.url )
-    else
-      flash[:alert] = t('.failure')
-
-    end
-    redirect_to @test_passage
-  end
-
   private
 
   def set_test_passage
