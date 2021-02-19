cc3821134d4f6635919e8eea69e81b41234e8dcb
KayumovELdar <eldar13121997@mail.ru>
2021-02-19 20:15:27 +0300

    Update gists_controller.rb

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -7,7 +7,7 @@ class GistsController < ApplicationController
       Gist.create!(question: @test_passage.current_question,
                    user: current_user,
                    url: result.gist_url)
-      flash[:notice] = t('.success', url: result[:html_url])
+      flash[:notice] = t('.success', link: result.gist_url)
     else
       flash[:alert] = t('.failure')
     end
