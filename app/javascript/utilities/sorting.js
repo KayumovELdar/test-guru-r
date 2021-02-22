fe1ff5cd19f75c092265c5035c02a8e5fcc4b9c2
KayumovELdar <eldar13121997@mail.ru>
2021-02-22 21:30:10 +0300

    fix

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -8,13 +8,9 @@ document.addEventListener('turbolinks:load', function () {
 
 function sortRowsByTitle() {
     var table = document.querySelector('table');
-
-    // NodeList
-    // https://developer.mozilla.org/ru/docs/Web/API/NodeList
     var rows = table.querySelectorAll('tr');
     var sortedRows = [];
 
-    // select all table rows except the first one which is the header
     for (var i = 1; i < rows.length; i++) {
         sortedRows.push(rows[i]);
     }
