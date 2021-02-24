852ea6b7c3035a4bacd0340d0347d44eb4f5e7e5
KayumovELdar <eldar13121997@mail.ru>
2021-02-24 13:07:09 +0300

    fix 13.1JS

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
-  const control = document.querySelector('.sort-by-title')
+  const control = document.getElementById('sort-by-title')
 
   if (control) {
     control.addEventListener('click', sortRowsByTitle)
@@ -7,24 +7,26 @@ document.addEventListener('turbolinks:load', function() {
 })
 
 function sortRowsByTitle() {
-  const table = document.querySelector('table')
+  const table = document.getElementById('table')
   const sortedRows = Array.from(table.rows).slice(1)
+  const arrowUp = document.querySelector('.octicon-arrow-up')
+  const arrowDown = document.querySelector('.octicon-arrow-down')
 
-  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+  if (arrowUp.classList.contains('hide')) {
 
     sortedRows
       .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1);
     rebuildTable(table, sortedRows)
-    this.querySelector('.octicon-arrow-up').classList.remove('hide')
-    this.querySelector('.octicon-arrow-down').classList.add('hide')
+    arrowUp.classList.remove('hide')
+    arrowDown.classList.add('hide')
 
   } else {
 
     sortedRows
       .sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? -1 : 1);
     rebuildTable(table, sortedRows)
-    this.querySelector('.octicon-arrow-down').classList.remove('hide')
-    this.querySelector('.octicon-arrow-up').classList.add('hide')
+    arrowUp.classList.add('hide')
+    arrowDown.classList.remove('hide')
   }
 }
 
