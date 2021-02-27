18ff46749aa31b1085978f6dc2a3fcd97b57de44
KayumovELdar <eldar13121997@mail.ru>
2021-02-27 20:34:14 +0300

    добавил вьюхи

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,2 +1,12 @@
 module BadgesHelper
+  def show_rule(badge)
+    case badge.rule_type
+    when "on_the_first_try"
+      "Прохождение теста с первого раза #{badge.rule_value}"
+    when "all_by_category"
+      "Категория закрыта #{badge.rule_value} "
+    when "all_by_level"
+      "Все теста данной сложности пройдены #{badge.rule_value} "
+    end
+  end
 end
